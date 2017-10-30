e074d93cc95ac178be3c93f757e942f2f8029cb2
Michael Vilabrera <m.vilabrera@gmail.com>
2017-10-30 11:35:31 -0400

    initial commit

---
+++ SnapSwift/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SnapSwift
+//
+//  Created by Michael Vilabrera on 10/30/17.
+//  Copyright © 2017 Michael Vilabrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sImageView: UIImageView!
+    @IBOutlet weak var nImageView: UIImageView!
+    @IBOutlet weak var aImageView: UIImageView!
+    @IBOutlet weak var pImageView: UIImageView!
+    @IBOutlet weak var jokerImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
